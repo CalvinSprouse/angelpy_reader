84093ccfefe0be75f8d7eda5c29d0ffdad8e8834
Calvin <sprousecal@gmail.com>
2023-07-26 14:21:30 -0500

    init

---
+++ web_reader.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Parses spacebattle forumns to extract text from TLA.
+
+Also includes functions to read from the website
+and search for new chapters (integrates to notifer).
+"""
+
+# imports
